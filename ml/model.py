304b8881d78b19251a0f89b9ebe5a37588d99c9c
Ethan Sayers <ethan418sayers@gmail.com>
2025-11-29 22:23:49 -0600

    Progress made on model.py

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,7 @@
 import pickle
-from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -19,8 +19,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+
+    model = RandomForestClassifier(n_estimators=100, random_state=99).fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -59,8 +60,9 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+
+    preds = model.predict(X)
+    return preds
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -117,12 +119,17 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+
+    df_slice = data[data[column_name] ==  slice_value]
+
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        df_slice,
+        label=label,
+        categorical_features=categorical_features,
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
